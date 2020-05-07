2b0ed96c992bcb7578e91225ff7d8d687119e9ea
croxana <croxana@student.21-school.ru>
2020-05-07 11:19:45 +0300

    create check for asm, edit parse

--- src_asm/parse_commands.c
+++ src_asm/parse_commands.c
@@ -12,6 +12,32 @@
 
 #include "../includes/asm.h"
 
+/* создаёт новый элемент списка команд, заполняет его нулями, ставит *prev,
+** при ошибке возвращает 0
+*/
+int			new_command(t_asm *asmb)
+{
+	t_command	*tmp;
+
+	tmp = NULL;
+	if (!asmb->comm)
+	{
+		if (!(asmb->comm = (t_command*)malloc(sizeof(t_command))))
+			return (error_line(ERR_MALLOC, NULL, 0));
+		asmb->comm_last = asmb->comm;
+	}
+	else
+	{
+		if (!(asmb->comm_last->next = (t_command*)malloc(sizeof(t_command))))
+			return (error_line(ERR_MALLOC, NULL, 0));
+		tmp = asmb->comm_last;
+		asmb->comm_last = asmb->comm_last->next;
+	}
+	ft_memset((void*)asmb->comm_last, 0, sizeof(t_command));
+	asmb->comm_last->prev = tmp;
+	return (1);
+}
+
 /* при ошибке возвращает 0
 */
 int			parse_commands(t_asm *asmb)
