72e3a6cda979680e6d92491463ed761cebde3069
tpepperm <tkarpukova@gmail.com>
2020-07-01 23:23:12 +0300

    add structs to disasm.h

--- includes/disasm.h
+++ includes/disasm.h
@@ -6,7 +6,7 @@
 /*   By: tkarpukova <tkarpukova@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 23:22:42 by tkarpukova        #+#    #+#             */
-/*   Updated: 2020/06/30 23:23:11 by tkarpukova       ###   ########.fr       */
+/*   Updated: 2020/07/01 23:22:44 by tkarpukova       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,28 @@
 
 # include "../libft/libft.h"
 # include "op.h"
+
+typedef struct 		s_arg
+{
+	int				type; //T_REG, T_IND, T_DIR
+	int 			arg; //значение аргумента
+	struct s_arg 	*next;
+}					t_arg;
+
+typedef struct  		s_op
+{
+	unsigned char		op_code; //код команды
+	t_arg				*args; //аргументы
+	struct s_command	*next;
+}						t_op;
+
+typedef struct          s_disasm
+{
+      char              *name;
+      char              *comment;
+      int               prog_length;
+      t_op				*ops; 
+}                       t_disasm;
+
 
 #endif
