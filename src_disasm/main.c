b120bbb29e8d475f63c6a18cc5a8ec6df0274e5f
tpepperm <tkarpukova@gmail.com>
2020-07-02 14:16:28 +0300

    edit structs, add check_filename.c

--- src_disasm/main.c
+++ src_disasm/main.c
@@ -6,31 +6,34 @@
 /*   By: tkarpukova <tkarpukova@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 23:19:46 by tkarpukova        #+#    #+#             */
-/*   Updated: 2020/06/30 23:42:59 by tkarpukova       ###   ########.fr       */
+/*   Updated: 2020/07/02 14:15:09 by tkarpukova       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/disasm.h"
 
-// int			check_filename(char *name, t_asm *asmb)
-// {
-// 	int		i;
-// 	int		length;
+int			check_filename(char *name)
+{
+	int			length;
+	t_disasm	disasm;
 
-// 	length = ft_strlen(name);
-// 	i = length - 1;
-// 	if (i < 1)
-// 		return (error_common(ERR_FILE_NAME));
-// 	if (name[i] == 's' && name[i - 1] == '.')
-// 	{
-// 		asmb->filename = ft_strnew(length + 2);
-// 		ft_strncpy(asmb->filename, name, (length - 2));
-// 		ft_strcat(asmb->filename, ".cor");
-// 	}
-// 	else
-// 		return (error_common(ERR_FILE_NAME));
-// 	return (1);
-// }
+	ft_memset(&disasm, 0, sizeof(disasm));
+	length = ft_strlen(name);
+	// проверяем, что у нас файл "*.cor" (то есть хотя бы  
+	// один символ и расширение .cor)
+	if (length < 5)
+		return (error_disasm(ERR_FILE_NAME));
+	if (ft_strcmp(&name[length - 4], ".cor") == 0)
+	{
+		disasm.filename = ft_strnew(length - 2);
+		ft_strncpy(disasm.filename, name, (length - 4));
+		ft_strcat(disasm.filename, ".s");
+	}
+	else
+		return (error_disasm(ERR_FILE_NAME));
+	// printf("FILENAME: %s\n", disasm.filename);
+	return (1);
+}
 
 int		main(int argc, char **argv)
 {
@@ -40,10 +43,13 @@ int		main(int argc, char **argv)
 		return (1);
 	}
 	// проверяем .cor
-	check_filename();
+	if (!check_filename(argv[1]))
+		return (1);
+
 	// парсим файл и записываем все в структуру
-	parse_file();
+	// parse_file();
+	
 	// записываем данные из структуры в файл
-	write_to_file();
+	// write_to_file();
 	return (0);
 }
